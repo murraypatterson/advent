acf291e048777cd55cc7855c98d6f5889fc2f21f
Murray Patterson <murray.patterson@gmail.com>
2022-01-02 23:56:03 -0400

    day 10

---
+++ day-10/day-10.py
@@ -0,0 +1,39 @@
+
+import sys
+
+def score(s) :
+
+    d = { '(':1, '[':2, '{':3, '<':4 }
+
+    t = 0
+    while s :
+        t *= 5
+        t += d[s.pop()]
+
+    return t
+
+p = { ')':'(', ']':'[', '}':'{', '>':'<' }
+d = { ')':3, ']':57, '}':1197, '>':25137 }
+
+t = 0
+v = []
+for line in open(sys.argv[1],'r') :
+
+    i = True
+    s = []
+    for c in line.strip() :
+
+        if c in p.values() :
+            s.append(c)
+
+        elif p[c] != s.pop() :
+            i = False
+
+            t += d[c]
+            break
+
+    if i :
+        v.append(score(s))
+        
+print(t)
+print(sorted(v)[len(v)//2])
