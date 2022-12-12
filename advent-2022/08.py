2b09d305b62e047a3c3af4f9dd8abba110b82a72
Murray Patterson <murray.patterson@gmail.com>
2022-12-12 01:30:58 -0500

    day 8 (generalize 8a)

--- advent-2022/08.py
+++ advent-2022/08.py
@@ -1,4 +1,57 @@
 import sys
+from math import prod
+
+# step onto a[i,j] wrt x and u
+def step(i, j, x, a, u, cs) :
+
+    if a[i][j] > x :
+        cs.add((i,j))
+        x = a[i][j]
+
+    if u : # for part b
+        if x < u :
+            if a[i][j] <= u : # make an exception
+                cs.add((i,j))
+
+        else : # x >= u, make it stop
+            x = 9
+
+    return cs, x
+
+# coords visible in a from origin o in direction d with upper bound u
+def coords(a, o, d, u = None) :
+    cs = set([])
+
+    n = len(a)
+    m = len(a[0])
+    
+    r, c = o
+    assert d in ['n','e','s','w']
+    x = -1
+
+    xs = a, u, cs
+    if d == 'n' :
+        for i in reversed(range(r)) :
+            cs, x = step(i, c, x, *xs)
+            
+    elif d == 'e' :
+        for j in range(c+1, m) :
+            cs, x = step(r, j, x, *xs)
+            
+    elif d == 's' :
+        for i in range(r+1, n) :
+            cs, x = step(i, c, x, *xs)
+            
+    elif d == 'w' :
+        for j in reversed(range(c)) :
+            cs, x = step(r, j, x, *xs)
+            
+    else :
+        assert False, d
+
+    return cs
+
+# Main
 
 a = []
 for line in open(sys.argv[1],'r') :
@@ -7,39 +60,27 @@ for line in open(sys.argv[1],'r') :
 n = len(a)
 m = len(a[0])
 
+# a
 cs = set([]) # set of (visible) coordinates to maintain
 
 for i in range(n) :
-
-    x = -1
-    for j in range(m) : # from the left..
-        
-        if a[i][j] > x :
-            cs.add((i,j))
-            x = a[i][j]
-
-    x = -1
-    for j in reversed(range(m)) : # from the right..
-
-        if a[i][j] > x :
-            cs.add((i,j))
-            x = a[i][j]
+    cs.update(coords(a, (i,-1), 'e')) # from the left..
+    cs.update(coords(a, (i,m), 'w')) # from the right..
 
 for j in range(m) :
+    cs.update(coords(a, (-1,j), 's')) # from the top..
+    cs.update(coords(a, (m,j), 'n')) # from the bottom..
 
-    x = -1
-    for i in range(n) : # from the top..
+# b
+b = 0
+for i in range(n) :
+    for j in range(m) :
 
-        if a[i][j] > x :
-            cs.add((i,j))
-            x = a[i][j]
+        s = []
+        for d in ['n','e','s','w'] :
+            s.append(len(coords(a, (i,j), d, a[i][j])))
 
-    x = -1
-    for i in reversed(range(n)) : # from the bottom..
-
-        if a[i][j] > x :
-            cs.add((i,j))
-            x = a[i][j]
-
-a = len(cs)
-print('08a:', a)
+        b = max(b, prod(s))
+        
+print('08a:', len(cs))
+print('08b:', b)
